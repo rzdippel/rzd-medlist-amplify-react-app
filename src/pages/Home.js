8f936c6b2926c496eaff51d57a37e359d01c5720
rzdippel <ryandippel22@gmail.com>
2024-01-21 20:15:42 -0500

    mainv1.1.9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const text = `
     Directions: 
     1. To sign up for an account, hit the "Exclusive Page" button in the top-right corner, it only requires an email!
-    2. Once logged in, you may select your medicines from our database and create a personalized list of medicines.`;
+    2. Once logged in, you may select your medicines from our database and create your personalized list of medicines.`;
 
     return (
         <div> 
